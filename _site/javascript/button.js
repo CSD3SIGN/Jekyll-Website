565da9b8a849c99074fc8b3082d038417c2af776
Craig Stewart <craig@ardlodge.co.uk>
2018-02-08 13:21:35 +0000

    Adding site files

---
+++ _site/javascript/button.js
@@ -0,0 +1,3 @@
+var buttonLines = '<span class="button-lines"> <div class="top"></div><div class="right"></div> <div class="bottom"></div><div class="left"></div></span>';
+
+$(".button").append(buttonLines);
